d22713e68da6126999c016e61ac00f309ef319b8
NAVEEN GOYAL -X (navgoyal - HCL TECHNOLOGIES LIMITED at Cisco) <navgoyal@cisco.com>
2023-12-10 15:09:12 +0530

    added login test

--- src/config/urls.js
+++ src/config/urls.js
@@ -10,6 +10,7 @@ isStandalone = window.navigator.userAgent.indexOf("Electron/") > -1;
 
 if (isStandalone) {
     domainName = "http://localhost:9093/api/v1";
+    
     //domainName = "https://developer-workboard-dev.cisco.com/api/v2";
     socketDomainName = "http://localhost:9093";
 } else {
@@ -18,6 +19,7 @@ if (isStandalone) {
         process.env.NODE_ENV !== "production"
     ) {
         domainName = "http://localhost:9093/api/v1";
+        domainName="https://onedemoserver.onrender.com/api/v1"
         //domainName = "https://developer-workboard-dev.cisco.com/api/v2";
         socketDomainName = "http://localhost:9093";
     } else {
@@ -26,10 +28,12 @@ if (isStandalone) {
             "//" +
             query.url.split("/")[2] +
             "/api/v1";
+            domainName="https://onedemoserver.onrender.com/api/v1"
         socketDomainName =
             query.url.split("/")[0] +
             "//" +
             query.url.split("/")[2];
+            socketDomainName="https://onedemoserver.onrender.com"
     }
 }
 
