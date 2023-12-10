b16b80bae9a7019eba4c1a55aefec59c07c4fa4c
NAVEEN GOYAL -X (navgoyal - HCL TECHNOLOGIES LIMITED at Cisco) <navgoyal@cisco.com>
2023-12-10 15:16:27 +0530

    added login test

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,5 +1,5 @@
-import qs from "query-string";
-const query = qs.parseUrl(window.location.href);
+// import qs from "query-string";
+// const query = qs.parseUrl(window.location.href);
 
 // API URL to be used based on environment
 let domainName;
@@ -23,16 +23,16 @@ if (isStandalone) {
         //domainName = "https://developer-workboard-dev.cisco.com/api/v2";
         socketDomainName = "http://localhost:9093";
     } else {
-        domainName =
-            query.url.split("/")[0] +
-            "//" +
-            query.url.split("/")[2] +
-            "/api/v1";
+        // domainName =
+        //     query.url.split("/")[0] +
+        //     "//" +
+        //     query.url.split("/")[2] +
+        //     "/api/v1";
             domainName="https://onedemoserver.onrender.com/api/v1"
-        socketDomainName =
-            query.url.split("/")[0] +
-            "//" +
-            query.url.split("/")[2];
+        // socketDomainName =
+        //     query.url.split("/")[0] +
+        //     "//" +
+        //     query.url.split("/")[2];
             socketDomainName="https://onedemoserver.onrender.com"
     }
 }
